ab7c4a8c1956440bf50bd5127e3ed612510e4167
wangfei <murolq@me.com>
2015-07-17 15:56:07 +0800

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 SampleApp::Application.routes.draw do
-
+  resources :users
   root to: 'static_pages#home'
-  match '/help',    to: 'static_pages#help'
-  match '/about',   to: 'static_pages#about'
-  match '/contact', to: 'static_pages#contact'
-  match '/signup', to: 'users#signup'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
