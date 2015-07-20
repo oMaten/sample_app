3b03d7b0531f937175688e25f3f172ad10f58762
wangfei <murolq@me.com>
2015-07-20 17:24:47 +0800

    Add finding users function and rebuild view.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,49 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
   def new
   	@user = User.new
   end
   def show
   	@user = User.find(params[:id])
   end
+  def index
+    @users = User.order(:id).page(params[:page]).per(15)
+  end
   def create
-    flash[:success] = "Welcome to Sample App"
   	@user = User.new(params[:user])
-    redirect_to @user
   	if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to Sample App"
       redirect_to @user
     else
       render 'new'
     end
   end
+  def edit
+  end
+  def update
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please login first."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user)
+        redirect_to(root_path)
+      end
+    end
 end
---
+++ lib/tasks/sample_data.rake
@@ -0,0 +1,18 @@
+namespace :db do
+  desc "Fill database with sample data"
+  task populate: :environment do
+    User.create!(name: "Example User",
+                 email: "example@railstutorial.org",
+                 password: "19820zxyCN",
+                 password_confirmation: "19820zxyCN")
+    99.times do |n|
+      name  = Faker::Name.name
+      email = "example-#{n+1}@railstutorial.org"
+      password = "password"
+      User.create!(name: name,
+                   email: email,
+                   password: password,
+                   password_confirmation: password)
+    end
+  end
+end
