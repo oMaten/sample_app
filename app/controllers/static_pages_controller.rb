53e00be56b8457af37bca8b0767751c08f0d6150
wangfei <murolq@me.com>
2015-07-24 12:48:40 +0800

    Add followed_user’s microposts in homepage but has not orderby.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-  end
+  	if signed_in?
+  		@microposts = Array.new
+  		@user = current_user
+  		@followed_users = @user.followed_users
+			@followed_users.each do |user|
+				@microposts.concat(user.microposts)
+			end
+			@paginatable_array = Kaminari.paginate_array(@microposts).page(params[:page]).per(15)
+		end
+	end
 
   def help
   end
